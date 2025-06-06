f0fbd8b169b2a2db9c3795b110df8b2ea5dca722
Tridip Karmakar <karmakartridip97@gmail.com>
2025-06-06 23:59:53 +0530

    practice basic weather app

--- backup-script.js
+++ backup-script.js
@@ -1,21 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   const cityInput = document.getElementById("city-input");
+
   const getWeatherBtn = document.getElementById("get-weather-btn");
-  const cityName = document.getElementById("city-name");
-  const temperature = document.getElementById("temperature");
-  const description = document.getElementById("description");
-  const error = document.getElementById("error-message");
+
   const weatherInfo = document.getElementById("weather-info");
-  const API_KEY = "916f8097d5f72d5a1d25c9dfe9a0991b";
+
+  const cityName = document.getElementById("city-name");
+
+  const temperature = document.getElementById("temperature");
+
+  const description = document.getElementById("description");
+
+  const errorMessage = document.getElementById("error-message");
+
+  const API_KEY = "916f8097d5f72d5a1d25c9dfe9a0991b"; //env variable
 
   getWeatherBtn.addEventListener("click", async () => {
     const city = cityInput.value.trim();
-    if (!city) return; // if the city name is blank then simple return nothing to do
+    if (!city) return;
 
-    // now we try to catch the details of the city
+    // it may throw an error
+    // server/database is always in another continent
 
     try {
       const weatherData = await fetchWeatherData(city);
+
       displayWeatherData(weatherData);
     } catch (error) {
       showError();
@@ -23,39 +32,42 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   async function fetchWeatherData(city) {
-    // gets the data
+    //gets the data
+
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
 
     const response = await fetch(url);
     console.log(typeof response);
-    console.log(response);
-
+    console.log("RESPONSE", response);
     if (!response.ok) {
       throw new Error("City Not found");
     }
 
     const data = await response.json();
-    console.log(data);
-
     return data;
   }
 
   function displayWeatherData(data) {
-    cityName.textContent = `City Name: ${data.name}`;
-    temperature.textContent = `Temperature: ${data.main.temp}`;
-    description.textContent = ` ${data.weather[0].description}`;
+    //display the data
 
-    let iconCode = data.weather[0].icon;
-    let iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+    console.log(data);
+    const { name, main, weather } = data;
 
-    document.getElementById("description-icon").src = iconUrl;
+    // Adds the data in the paragraph
+    cityName.textContent = `City Name: ${name}`;
 
+    temperature.textContent = `Temperature: ${main.temp}`;
+
+    description.textContent = `Weather: ${weather[0].description}`;
+
+    // unlock the display
     weatherInfo.classList.remove("hidden");
-    error.classList.add("hidden");
+    errorMessage.classList.add("hidden");
   }
 
   function showError() {
     weatherInfo.classList.add("hidden");
-    error.classList.remove("hidden");
+
+    errorMessage.classList.remove("hidden");
   }
 });
